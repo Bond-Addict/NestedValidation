d685b6b430ae006c178d342167ce57fca21064ab
Bond-Addict <117455332+Bond-Addict@users.noreply.github.com>
2025-02-01 22:50:10 -0500

    Added actual blazor server

---
+++ NestedValidationBlazorServer/Validators/AddressValidator.cs
@@ -0,0 +1,13 @@
+﻿using FluentValidation;
+using NestedValidation.Models;
+
+namespace NestedValidation.Validators
+{
+    public class AddressValidator : AbstractValidator<Address>
+    {
+        public AddressValidator()
+        {
+            RuleFor(a => a.Street).NotEmpty();
+        }
+    }
+}
